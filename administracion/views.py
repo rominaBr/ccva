1c15336b62167cc9e7e33a57812d9c69e3274ec2
Romina <romina.brutti@gmail.com>
2022-08-28 06:24:00 -0300

    statics

--- administracion/views.py
+++ administracion/views.py
@@ -1,5 +1,4 @@
 from django.shortcuts import render
-from django.views.generic import ListView
 from django.core.paginator import Paginator
 from .models import Categoria
 from posts.models import Post
--- posts/models.py
+++ posts/models.py
@@ -1,5 +1,4 @@
 import os
-from tkinter import N
 from django.db import models
 from django.db.models.signals import pre_save
 from django.utils.text import slugify
