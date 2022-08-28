1d1a90a1ea80cea83f908374a6c686d0f44c3954
Romina <romina.brutti@gmail.com>
2022-08-28 12:24:18 -0300

    likes y dislikes

--- blog/settings/produccion.py
+++ blog/settings/produccion.py
@@ -23,8 +23,8 @@ DATABASES = {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
             'NAME': 'd41iq1opootdjr',
             'USER': 'oagprslwacopuy',
-            'PASSWORD': '84730f5c7c2db2fc6a6d1a3377a0c57f31c96f8118c26d676c170da6287512d9',
-            'HOST': 'bc502c76413bf9190c32b0bf2605eea2b3dafe3b456522bec6876c5b8acc7ce6',
+            'PASSWORD': 'bc502c76413bf9190c32b0bf2605eea2b3dafe3b456522bec6876c5b8acc7ce6',
+            'HOST': 'ec2-44-195-100-240.compute-1.amazonaws.com',
             'PORT': 5432,           
             
         },
