504d75f85887c52c4a6558937df67c8862b4379c
Romina <romina.brutti@gmail.com>
2022-09-02 23:44:36 -0300

    config

--- blog/settings/base.py
+++ blog/settings/base.py
@@ -6,15 +6,6 @@ from decouple import config
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config('SECRET_KEY')
-# SECURITY WARNING: don't run with debug turned on in production!
-
-
-
 # Application definition
 
 INSTALLED_APPS = [
@@ -147,13 +138,7 @@ CKEDITOR_CONFIGS = {
             },
 }
 
-#email smtp:
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_HOST = "smtp-mail.outlook.com"
-EMAIL_USE_TLS = True
-EMAIL_PORT = 587
-EMAIL_HOST_USER="ccva_prueba@outlook.com"
-EMAIL_HOST_PASSWORD = "Villaangelaprueba2022"
+
 
 from django.contrib.staticfiles import storage
 import functools
